3d2bf656408e9c4e04032bf3d11d85b67cfa4648
Minh Nguyen <minh_nguyen@epam.com>
2022-12-24 02:55:42 +0700

    set up intial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import './App.css';
+import { Header } from './container';
+import { Navbar } from './components';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { RiMenu2Line, RiCloseLine } from 'react-icons/ri';
+
+const index = () => {
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
