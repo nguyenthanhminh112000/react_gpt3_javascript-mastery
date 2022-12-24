d42ecf2ab5177e3a92d0abf0556345e43897a9e1
Minh Nguyen <minh_nguyen@epam.com>
2022-12-24 11:31:47 +0700

    Resolve load svg issue

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { RiMenu2Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
+import './index.css';
+console.log('src/components/Navbar/index.tsx outside');
 
 const index = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src="" alt="" />
+          <img src={logo as any} alt="logo" />
         </div>
       </div>
     </div>
